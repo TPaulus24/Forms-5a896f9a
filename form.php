125f518b9cb3dfce4598141444db6c5410a7f8a3
FIRST_TOM LAST_PAULUS <54597104+TPaulus24@users.noreply.github.com>
2019-11-20 10:32:59 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bit Academy</title>
+</head>
+<body>
+<form action="form.php" method="get" class="form-example">
+  <div class="form-example">
+    <label for="email">Voer E-mail in </label>
+    <input type="email" name="email" id="email" required placeholder="enter email">
+  </div>
+  <div class="form-example">
+    <input type="submit" value="sent">
+  </div>
+</form>
+</body>
+</html>
